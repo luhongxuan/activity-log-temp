42e3754d0a3f729bb8e699470c01fff63361d37b
HansLu2025 <B11209013@gapps.ntust.edu.tw>
2025-10-10 11:34:27 +0800

    test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,10 @@
-def add(a, b):
-    return a + b
+def check_sign(n):
+    if n > 0:
+        return "positive"
+    elif n < 0:
+        return "negative"
+    else:
+        return "zero"
 
-def test_add():
-    assert add(2, 3) == 5
+def test_check_sign():
+    assert check_sign(10) == "positive"
